d9e94ce23d22f4d2a72ded21a8cc1b721d15fd5f
star64812 <star64812@naver.com>
2016-10-15 15:10:20 +0900

    day1 파이썬 코드 실습

---
+++ turtle1.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(4)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+
+
+
---
+++ turtle2.py
@@ -0,0 +1,26 @@
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(4)
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(140)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(40)
+             
+
+
+
